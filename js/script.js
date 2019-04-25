b18d7740dff00b19f541e6b3112215e35294c125
Galen <galencasstevens@gmail.com>
2019-04-25 01:51:05 -0400

    getting some JS action going

--- js/script.js
+++ js/script.js
@@ -11,9 +11,18 @@ var turn;
 window.onload = init;
 
 function init() {
+  var board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+  ]
   slots = document.getElementsByClassName('slot');
   col1 = document.getElementsByClassName('col1');
-
+  turn = 'player1';
+/*
   for(var i = 0; i < col1.length; i++) {
     col1[i].children[0].onclick = function(eventObj) {
       var slot = eventObj.target;
@@ -27,6 +36,43 @@ function init() {
         slot.children[0].classList.add('player1');
     }
   }
+*/
 
+  for(var i = 0; i < col1.length; i++) {
+    col1[i].children[0].onclick = function() {
+        move('col1');
+    }
+  }
+/*
+  for(var i = 0; i < col1.length; i++) {
+    col1[i].onclick = function() {
+        move('col1');
+    }
+  }
+*/
+  for(var i = 0; i < col2.length; i++) {
+    col2[i].children[0].onclick = function() {
+        move('col2');
+    }
+  }
+/*
+  for(var i = 0; i < col2.length; i++) {
+    col2[i].onclick = function() {
+        move('col2');
+    }
+  }
+*/
+  function move(column) {
+    if(turn === 'player1' && column === 'col1') {
+      slots[35].classList.add('player1');
+    } else if(turn === 'player2' && column === 'col1') {
+      slots[28].classList.add('player2');
+    }
 
+    if(turn === 'player1') {
+      turn = 'player2';
+    } else if(turn === 'player2') {
+      turn = 'player1';
+    }
+  }
 }
